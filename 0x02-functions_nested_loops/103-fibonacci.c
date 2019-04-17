fb3217b1ecab96f80db1dc0c0529d32ac50d74d9
Russell Molimock <38747720+Rmolimock@users.noreply.github.com>
2019-04-17 13:15:17 -0700

    redo without recursion, simple loop instead

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,40 +1,27 @@
 #include <stdio.h>
 
-long print_fib(long prev, long curr, long sum);
-
 /**
- *main - Entry point.
- *
- *Return: 0 on success.
- */
-int main(void)
-{
-	int i = 2;
-
-	/* this loop is strictly to satisfy the test program at */
-	/*at holberton school, which expects the use of at least */
-	/*one loop. I used recursion to make it simpler */
-	while (i > 0)
-		i--;
-	return (print_fib(1, 2, 0));
-}
-
-/**
- *print_fib - print sum of even fibinacci numbers < 4million
+ *main - print sum of even fibinacci numbers < 4million
  *@prev: previous number in sequence
  *@curr: current number in sequence
  *@sum: sum of even numbers in sequence thusfar
  *
  *Return: recursively count up until next number > 4million
  */
-long print_fib(long prev, long curr, long sum)
+void main(void)
 {
-	if (!(curr % 2))
-		sum += curr;
-	if ((prev + curr) > 4000000)
+	long prev = 1;
+	long curr = 2;
+	long tmp = 0;
+	long sum = 0;
+
+	while (curr < 4000000)
 	{
-		printf("%ld\n", sum);
-		return (0);
+		if (!(curr % 2))
+			sum += curr;
+		tmp = curr + prev;
+		prev = curr;
+		curr = tmp;
 	}
-	return (print_fib(curr, (prev + curr), sum + 1));
+	printf("%ld\n", sum);
 }
