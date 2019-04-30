0f54014050ebbaaa6be074eec0daed4948f1babb
Rmolimock <495@holbertonschool.com>
2019-04-30 00:25:31 +0000

    null check buffer itself while finding length

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (!filename)
 		return (-1);
-	while (text_content[l])
+	while (text_content && text_content[l])
 		l++;
 	buf = malloc(sizeof(char) * (l + 1));
 	while (text_content[i])
