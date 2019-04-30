67d19d0fc3ef1054dd5c48a1094e11b4612359dc
Rmolimock <495@holbertonschool.com>
2019-04-30 00:33:24 +0000

    return 1 if no buffer given

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -15,38 +15,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	char *buf;
-	int l = 0;
 	int i = 0;
 	int fn;
 	int wr;
 
-	if (text_content == NULL)
-		return (-1);
 	if (!filename)
 		return (-1);
-	while (text_content && text_content[l])
-		l++;
-	buf = malloc(sizeof(char) * (l + 1));
-	while (text_content[i])
-	{
-		buf[i] = text_content[i];
-		i++;
-	}
 	fn = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fn == -1)
-	{
-		free(buf);
 		return (-1);
-	}
-	wr = write(fn, buf, l);
+
+	while (text_content && text_content[i])
+		i++;
+	wr = write(fn, text_content, i);
+	close (fn);
 	if (wr < 0)
-	{
-		free(buf);
-		wr = close(fn);
 		return (-1);
-	}
-	free(buf);
-	wr = close(fn);
 	return (1);
 }
