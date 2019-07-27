f167b08dfb555bc4ab2521ba1b5d411cafca743d
Rmolimock <495@holbertonschool.com>
2019-07-27 04:37:06 +0000

    perform function on each node

---
+++ 0x1D-binary_trees/7-binary_tree_inorder.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ *binary_tree_inorder - perform a function on nodes in a binary tree
+ *@tree: binary tree to be traversed
+ *@func: the function to be called on each node
+ *
+ *Return: Void.
+ */
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (!tree || !func)
+		return;
+	if (tree)
+	{
+		binary_tree_inorder(tree->left, func);
+		func(tree->n);
+		binary_tree_inorder(tree->right, func);
+	}
+}
